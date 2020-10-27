32f293134aebcc9b1621c7dddf8ef3d8e74127bb
WakkoStar <hugo.delpia@canal-plus.com>
2020-10-27 14:34:49 +0100

    improve dashboard

--- src/components/Workingtime/Dashboard.vue
+++ src/components/Workingtime/Dashboard.vue
@@ -1,109 +1,29 @@
 <template>
   <div class="container-dashboard">
     <div id="clock-container">
-      <div id="clock">
-        <button class="btn">START SESSION</button>
-        <p>Session finished at 17:00:00</p>
-      </div>
+      <Clock />
     </div>
     <div id="calendar">
       <h2>Working times</h2>
-      <ul>
-        <li>
-          <div class="date">
-            <h3 class="day">26</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 17:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">25</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">24</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">23</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">22</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">21</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">20</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>14:30 → 18:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-        <li>
-          <div class="date">
-            <h3 class="day">19</h3>
-            <h3 class="month">Oct</h3>
-          </div>
-          <div class="session">
-            <p>8:30 → 12:30</p>
-            <p>14:30 → 18:30</p>
-            <p>18:30 → 21:30</p>
-            <p class="add-link">Ajouter une session</p>
-          </div>
-        </li>
-      </ul>
+      <Calendar />
     </div>
   </div>
 </template>
 
 <script>
+import Clock from "./Clock";
+import Calendar from "./Calendar";
 export default {
   name: "Dashboard",
+  components: {
+    Clock,
+    Calendar,
+  },
+  methods: {
+    ok() {
+      console.log("ok");
+    },
+  },
 };
 </script>
 
@@ -120,7 +40,6 @@ export default {
       background-color: #1f2224;
       border-radius: 2vw;
       padding: 3vw 1vw;
-
       button {
         border-radius: 20px;
         background-color: #dfc824;
@@ -165,7 +84,6 @@ export default {
       li {
         display: flex;
         min-height: 6vw;
-        justify-content: center;
         border-bottom: 1px solid #161617;
         align-items: center;
         &:last-child {
