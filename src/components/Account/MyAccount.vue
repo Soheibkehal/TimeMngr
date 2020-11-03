b51a9e398dfd2afbe46051b673f39f1d6b1b22b3
Banaanaa <39826202+Banaanaa@users.noreply.github.com>
2020-11-03 11:23:22 +0100

    Add files via upload
    teams + myaccount front

---
+++ src/components/Account/MyAccount.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <br><br><br>
+    <h1><img src="../../img/avatar_2.png" height="50px" width="50px" alt=""/>&emsp; &#160;My Account</h1>
+    <br>
+    <div class="overlay-container">
+      <form>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Name" style="text-align:left" >
+            <p><b>Name :</b> Vanissa</p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Lname" style="text-align:left" >
+            <p><b>Last Name :</b> Balakichenane</p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Email" style="text-align:left" >
+            <p><b>Email :</b> epitech@epitech.eu</p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Role" style="text-align:left" >
+            <p><b>Role :</b> Nothing</p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Team" style="text-align:left" >
+            <p><b>Team :</b> Nothing</p>
+          </div>        
+        </div>
+        <button type="submit" class="btn btn-primary">Disconnect</button>
+      </form>
+    </div>
+    <br><br><br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyAccount",
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vw;
+  width: 50vw;
+  height: 30vw;
+  border-radius: 2vw;
+  overflow: hidden;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.2);
+  background-color: #1f2224;
+;
+}
+.container h1 {
+  font-size: 1vw;
+  font-weight: bold;
+  color: white;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin: 1vw 0 2vw;
+  font-family: 'Montserrat', sans-serif;
+}
+.overlay-container {
+  top: 8vw;
+  left: 10vw;
+  width: 20vw;
+}
+button {
+  border-radius: 20px;
+  background-color: #dfc824;
+  color: #fff;
+  margin-top : 1.5vw;
+  font-size: 1vw;
+  font-weight: bold;
+  padding: 10px 40px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  border: 0;
+  font-family: 'Montserrat', sans-serif;
+}
+.form-control{
+  background-color: transparent;
+  color : white;
+  border: 0;
+  border-bottom: 2px solid #dfc824;
+}
+input::placeholder{
+  color : #dfc824;
+}
+input{
+  background-color:white;
+}
+</style>
---
+++ src/components/Account/Teams.vue
@@ -0,0 +1,230 @@
+<template>
+    <div class="overlay-container">
+    <div class="containerg">
+    <h2><img src="../../img/avatar_2.png" height="50px" width="50px" alt=""/>&emsp; &#160;Teams Vanissa</h2>
+    <br>
+      <form>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Name" style="text-align:left" >
+            <p><b>Vanissa</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Lname" style="text-align:left" >
+            <p><b>Soheib</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Email" style="text-align:left" >
+            <p><b>Hugo</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Role" style="text-align:left" >
+            <p><b>Marie</b></p>
+          </div>        
+        </div>
+        <button type="submit" class="btn btn-primary">Edit</button>
+      </form>
+    </div>
+    <div class="containerd">
+    <h2><img src="../../img/avatar_2.png" height="50px" width="50px" alt=""/>&emsp; &#160;Teams Soheib</h2>
+    <br>
+      <form>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Name" style="text-align:left" >
+            <p><b>Vanissa</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Lname" style="text-align:left" >
+            <p><b>Soheib</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Email" style="text-align:left" >
+            <p><b>Hugo</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Role" style="text-align:left" >
+            <p><b>Marie</b></p>
+          </div>        
+        </div>
+        <button type="submit" class="btn btn-primary">Edit</button>
+      </form>
+    </div>
+    <br>
+    <div class="containergb">
+    <h2><img src="../../img/avatar_2.png" height="50px" width="50px" alt=""/>&emsp; &#160;Teams Hugo</h2>
+    <br>
+      <form>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Name" style="text-align:left" >
+            <p><b>Vanissa</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Lname" style="text-align:left" >
+            <p><b>Soheib</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Email" style="text-align:left" >
+            <p><b>Hugo</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Role" style="text-align:left" >
+            <p><b>Marie</b></p>
+          </div>        
+        </div>
+        <button type="submit" class="btn btn-primary">Edit</button>
+      </form>
+    </div>
+    <div class="containerdb">
+    <h2><img src="../../img/avatar_2.png" height="50px" width="50px" alt=""/>&emsp; &#160;Teams Marie</h2>
+    <br>
+      <form>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Name" style="text-align:left" >
+            <p><b>Vanissa</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Lname" style="text-align:left" >
+            <p><b>Soheib</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Email" style="text-align:left" >
+            <p><b>Hugo</b></p>
+          </div>        
+        </div>
+        <div class="form-group">
+          <div class="form-control" placeholderr="Role" style="text-align:left" >
+            <p><b>Marie</b></p>
+          </div>        
+        </div>
+        <button type="submit" class="btn btn-primary">Edit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Teams",
+};
+</script>
+
+<style scoped>
+.containerg {
+  position: relative;
+  display: flex;
+  top: 0vw;
+  left: 10vw;
+  width: 20vw;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vw;
+  width: 30vw;
+  height: 30vw;
+  border-radius: 2vw;
+  overflow: hidden;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.2);
+  background-color: #283036;
+}
+.containerd {
+  position: relative;
+  display: flex;
+  top: -32vw;
+  left : 50vw;
+  width: 20vw;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vw;
+  width: 30vw;
+  height: 30vw;
+  border-radius: 2vw;
+  overflow: hidden;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.2);
+  background-color: #283036;
+;
+}
+.containergb {
+  position: relative;
+  display: flex;
+  top: -30vw;
+  left: 10vw;
+  width: 20vw;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vw;
+  width: 30vw;
+  height: 30vw;
+  border-radius: 2vw;
+  overflow: hidden;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.2);
+  background-color: #283036;
+}
+.containerdb {
+  position: relative;
+  display: flex;
+  top: -62vw;
+  left: 50vw;
+  width: 20vw;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vw;
+  width: 30vw;
+  height: 30vw;
+  border-radius: 2vw;
+  overflow: hidden;
+  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2), 0px 10px 10px rgba(0, 0, 0, 0.2);
+  background-color: #283036;
+}
+.container h1 {
+  font-size: 1vw;
+  font-weight: bold;
+  color: white;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin: 1vw 0 2vw;
+  font-family: 'Montserrat', sans-serif;
+}
+.overlay-container {
+  top: 8vw;
+  left: 10vw;
+  width: 20vw;
+}
+button {
+  border-radius: 20px;
+  background-color: #dfc824;
+  color: #fff;
+  margin-top : 1.5vw;
+  font-size: 1vw;
+  font-weight: bold;
+  padding: 10px 40px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  border: 0;
+  font-family: 'Montserrat', sans-serif;
+}
+.form-control{
+  background-color: transparent;
+  color : white;
+  border: 0;
+  border-bottom: 2px solid #dfc824;
+}
+input::placeholder{
+  color : #dfc824;
+}
+input{
+  background-color:white;
+}
+</style>
