806f11e4b4c99d47f7192cc3a993580cfb349f8d
Banaanaa <39826202+Banaanaa@users.noreply.github.com>
2020-11-03 11:24:31 +0100

    Add files via upload
    app.vue update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
+      <router-link to="/teams">Teams</router-link> |
+      <router-link to="/myaccount">My Account</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/">Dashboard</router-link> |
   
